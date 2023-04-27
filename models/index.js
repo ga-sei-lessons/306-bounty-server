0ad731cb9750d41f53c7e18e0b1c585a4d366344
weston-bailey <wdbailey87@gmail.com>
2023-04-27 14:35:14 -0700

    init commit

---
+++ models/index.js
@@ -0,0 +1,17 @@
+// require the mongoose package
+const mongoose = require('mongoose')
+
+// connect to a mongoDB URI
+const dbName = "bountyApi"
+const uri = 'mongodb://127.0.0.1/' + dbName
+mongoose.connect(uri)
+
+// config console logs for mongoose connection
+const db = mongoose.connection
+db.once('open', () => console.log(`connect to mongodb on ${db.host}:${db.port} 🤠`))
+db.on('error', err => console.log(`ut oh speghtettio 🍝`, err))
+
+// export all models
+module.exports = {
+    Bounty: require('./Bounty')
+}
